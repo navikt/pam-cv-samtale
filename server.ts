8a2710dffcf65d13b4c582300dd18b2e04741fed
Aksel Wester <aksel@wester.co>
2019-06-27 14:53:07 +0200

    Legger til server.ts

---
+++ server.ts
@@ -0,0 +1,26 @@
+import * as express from 'express';
+import * as helmet from 'helmet';
+import * as path from 'path';
+
+const server = express();
+
+// security
+server.disable('x-powered-by');
+server.use(helmet());
+
+// health checks
+server.get('/cv/samtale/internal/isAlive', (req, res) => res.sendStatus(200));
+server.get('/cv/samtale/internal/isReady', (req, res) => res.sendStatus(200));
+
+server.use('/cv/samtale/static', express.static(path.resolve(__dirname, 'dist')));
+server.use(
+    '/cv/samtale*',
+    (req: express.Request, res: express.Response) => {
+        res.sendFile(path.resolve(__dirname, 'dist', 'index.html'));
+    }
+);
+
+const port = process.env.PORT || 8080;
+server.listen(port, () => {
+    console.log('Server listening on port ', port);
+});
