0ecfb71dbb1a8264eadc8275163ef8b121df0816
Aksel Wester <aksel@wester.co>
2019-07-15 13:58:55 +0200

    Gjør masse endringer for å få proxyen til å fungere
    Endringen som faktisk fikk det til å fungerer var å fjerne server.use(express.json()); som vi brukte til log-endepunktet. Det var den linjen som gjorde at bodyen i requesten til proxyen ble skrevet om, og at derfor content-length ble feil. Hvis jeg har rett så er det også mulig at denne bugen som jeg var innom er forårsaket av samme feil: https://www.bountysource.com/issues/60143349-pipe-does-not-seem-to-call-end

--- server.ts
+++ server.ts
@@ -1,7 +1,9 @@
 import * as express from 'express';
+import { Request} from 'express';
 import * as proxy from 'express-http-proxy';
 import * as helmet from 'helmet';
 import * as path from 'path';
+import { RequestOptions } from 'http';
 
 if (!process.env.PAM_CV_API_PROXY_KEY) {
     throw new Error("Miljøvariabel PAM_CV_API_PROXY_KEY er ikke satt");
@@ -18,7 +20,7 @@ const MILJOVARIABLER = {
 console.log(`API_GATEWAY_HOST: ${MILJOVARIABLER.API_GATEWAY_HOST}`);
 
 const server = express();
-server.use(express.json());
+// server.use(express.json());
 
 // security
 server.disable('x-powered-by');
@@ -28,14 +30,6 @@ server.use(helmet());
 server.get('/cv-samtale/internal/isAlive', (req, res) => res.sendStatus(200));
 server.get('/cv-samtale/internal/isReady', (req, res) => res.sendStatus(200));
 
-server.post('/cv-samtale/log', (req, res) => {
-    console.log(JSON.stringify({
-        ...req.body,
-        level: "Error"
-    }));
-    res.sendStatus(200);
-});
-
 const getCookie = (name: string, cookie: string) => {
     const re = new RegExp(`${name}=([^;]+)`);
     const match = re.exec(cookie);
@@ -44,22 +38,34 @@ const getCookie = (name: string, cookie: string) => {
 
 server.use(
     '/cv-samtale/api',
-    proxy(MILJOVARIABLER.API_GATEWAY_HOST, {
-        https: true,
-        proxyReqOptDecorator: (proxyReqOpts: any, srcReq: any) => ({
-            ...proxyReqOpts,
-            cookie: srcReq.headers.cookie,
-            headers: {
-                ...proxyReqOpts.headers,
-                'X-XSRF-TOKEN': getCookie('XSRF-TOKEN', srcReq.headers.cookie),
-                'x-nav-apiKey': MILJOVARIABLER.PROXY_API_KEY
-            }
-        }),
+    proxy('http://pam-cv', {
+        // https: true,
+        // proxyReqOptDecorator: (proxyReqOpts: RequestOptions, srcReq: Request)  => {
+        //     // proxyReqOpts.headers['Cookie'] = srcReq.header('Cookie');
+        //     proxyReqOpts.headers['X-XSRF-TOKEN'] = getCookie('XSRF-TOKEN', srcReq.header('Cookie'));
+        //     proxyReqOpts.headers['x-nav-apiKey'] = MILJOVARIABLER.PROXY_API_KEY;
+        //     return proxyReqOpts;
+        // },
+        // proxyReqPathResolver: (req: any) => {
+        //     const p = req.originalUrl.replace(new RegExp('/cv-samtale/api'), '/pam-cv-api/pam-cv-api');
+        //     console.log(JSON.stringify({message: `New path: ${MILJOVARIABLER.API_GATEWAY_HOST}${p}`}))
+        //     return p;
+        // }
         proxyReqPathResolver: (req: any) => (
-            req.originalUrl.replace(new RegExp('/cv-samtale/api'), '/pam-cv-api/pam-cv-api')
-        )
+            req.originalUrl.replace(new RegExp('/cv-samtale/api'), '/cv/api')
+        ),
+        parseReqBody: true,
+        proxyReqBodyDecorator: (bodyContent) => bodyContent
     })
 );
+
+// server.post('/cv-samtale/log', (req, res) => {
+//     console.log(JSON.stringify({
+//         ...req.body,
+//         level: "Error"
+//     }));
+//     res.sendStatus(200);
+// });
 
 server.use('/cv-samtale/static', express.static(path.resolve(__dirname, 'dist')));
 server.use(
