b0356934ff7546b02acdd39a9a2a60f0ede362ac
Aksel Wester <aksel@wester.co>
2019-07-23 16:32:00 +0200

    Legger til skrive-indikator-animasjon, og logikk i MeldingsLogg for å ta vare på meldinger som ikke skal være synlig ennå

--- src/MeldingsLogg.elm
+++ src/MeldingsLogg.elm
@@ -1,4 +1,18 @@
-module MeldingsLogg exposing (MeldingsLogg, init, leggTilSpørsmål, leggTilSvar, meldinger)
+module MeldingsLogg exposing
+    ( FerdigAnimertMeldingsLogg
+    , FerdigAnimertStatus(..)
+    , MeldingsLogg
+    , SkriveStatus(..)
+    , ferdigAnimert
+    , fullførMelding
+    , init
+    , leggTilSpørsmål
+    , leggTilSvar
+    , meldinger
+    , skriveStatus
+    , startÅSkrive
+    , tilMeldingsLogg
+    )
 
 import Melding exposing (Melding)
 
@@ -7,25 +21,150 @@ type MeldingsLogg
     = MeldingsLogg MeldigsLoggInfo
 
 
+type FerdigAnimertMeldingsLogg
+    = FerdigAnimertMeldingsLogg (List Melding)
+
+
 type alias MeldigsLoggInfo =
-    List Melding
+    { meldingsLogg : List Melding
+    , ikkeVist : UskrevneMeldinger
+    }
+
+
+type UskrevneMeldinger
+    = Ingen
+    | SkriverEnMelding Melding (List Melding)
+    | LeserMelding Melding (List Melding)
+
+
+type SkriveStatus
+    = Skriver
+    | SkriverIkke
 
 
 init : MeldingsLogg
 init =
-    MeldingsLogg []
+    MeldingsLogg
+        { meldingsLogg = []
+        , ikkeVist = Ingen
+        }
 
 
 meldinger : MeldingsLogg -> List Melding
-meldinger (MeldingsLogg meldingsLogg) =
+meldinger (MeldingsLogg { meldingsLogg }) =
     meldingsLogg
 
 
+skriveStatus : MeldingsLogg -> SkriveStatus
+skriveStatus (MeldingsLogg { ikkeVist }) =
+    case ikkeVist of
+        Ingen ->
+            SkriverIkke
+
+        SkriverEnMelding _ _ ->
+            Skriver
+
+        LeserMelding _ _ ->
+            SkriverIkke
+
+
 leggTilSpørsmål : List Melding -> MeldingsLogg -> MeldingsLogg
-leggTilSpørsmål nyeMeldinger (MeldingsLogg meldingsliste) =
-    MeldingsLogg (meldingsliste ++ nyeMeldinger)
+leggTilSpørsmål nyeMeldinger (MeldingsLogg info) =
+    MeldingsLogg
+        { info
+            | ikkeVist =
+                case nyeMeldinger of
+                    first :: rest ->
+                        case info.ikkeVist of
+                            Ingen ->
+                                LeserMelding first rest
+
+                            SkriverEnMelding melding list ->
+                                SkriverEnMelding melding (list ++ nyeMeldinger)
+
+                            LeserMelding melding list ->
+                                LeserMelding melding (list ++ nyeMeldinger)
+
+                    [] ->
+                        Ingen
+        }
 
 
 leggTilSvar : Melding -> MeldingsLogg -> MeldingsLogg
-leggTilSvar nyMelding (MeldingsLogg meldingsliste) =
-    MeldingsLogg (meldingsliste ++ [ nyMelding ])
+leggTilSvar nyMelding (MeldingsLogg info) =
+    MeldingsLogg { info | meldingsLogg = info.meldingsLogg ++ [ nyMelding ] }
+
+
+startÅSkrive : MeldingsLogg -> MeldingsLogg
+startÅSkrive (MeldingsLogg info) =
+    MeldingsLogg
+        { info
+            | ikkeVist =
+                case info.ikkeVist of
+                    Ingen ->
+                        Ingen
+
+                    SkriverEnMelding melding list ->
+                        SkriverEnMelding melding list
+
+                    LeserMelding melding list ->
+                        SkriverEnMelding melding list
+        }
+
+
+fullførMelding : MeldingsLogg -> MeldingsLogg
+fullførMelding (MeldingsLogg info) =
+    MeldingsLogg
+        { info
+            | meldingsLogg =
+                case info.ikkeVist of
+                    Ingen ->
+                        info.meldingsLogg
+
+                    SkriverEnMelding melding _ ->
+                        info.meldingsLogg ++ [ melding ]
+
+                    LeserMelding _ _ ->
+                        info.meldingsLogg
+            , ikkeVist =
+                case info.ikkeVist of
+                    Ingen ->
+                        Ingen
+
+                    SkriverEnMelding _ list ->
+                        case list of
+                            first :: rest ->
+                                LeserMelding first rest
+
+                            [] ->
+                                Ingen
+
+                    LeserMelding melding list ->
+                        LeserMelding melding list
+        }
+
+
+type FerdigAnimertStatus
+    = FerdigAnimert FerdigAnimertMeldingsLogg
+    | MeldingerGjenstår
+
+
+ferdigAnimert : MeldingsLogg -> FerdigAnimertStatus
+ferdigAnimert (MeldingsLogg info) =
+    case info.ikkeVist of
+        Ingen ->
+            FerdigAnimert (FerdigAnimertMeldingsLogg info.meldingsLogg)
+
+        SkriverEnMelding melding list ->
+            MeldingerGjenstår
+
+        LeserMelding melding list ->
+            MeldingerGjenstår
+
+
+tilMeldingsLogg : FerdigAnimertMeldingsLogg -> MeldingsLogg
+tilMeldingsLogg (FerdigAnimertMeldingsLogg meldingslogg) =
+    MeldingsLogg
+        { meldingsLogg = meldingslogg
+        , ikkeVist = Ingen
+        }
