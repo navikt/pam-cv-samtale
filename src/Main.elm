43f085d84ad6929a5ff1516c3e72406fbd3ad863
Aigars Tumanis <aigar.tumanis@nav.no>
2019-08-06 12:20:10 +0200

    Småendringer etter samtale med designer

--- src/Main.elm
+++ src/Main.elm
@@ -46,6 +46,10 @@ type alias RegistreringsProgresjon =
     }
 
 
+stjerne =
+    "⭐"
+
+
 type Seksjonsstatus
     = IkkeBegynt
     | Begynt
@@ -337,11 +341,13 @@ initialiserSamtale : SamtaleSeksjon
 initialiserSamtale =
     MeldingsLogg.init
         |> MeldingsLogg.leggTilSpørsmål
-            [ Melding.spørsmål [ "Hei" ]
+            [ Melding.spørsmål [ "Hei, jeg er roboten CVert" ]
             , Melding.spørsmål
                 [ "Velkommen til CV-registrering!"
                 , "Det vi skal gjennom nå er utdanning, arbeidserfaring, språk og sammendrag."
-                , "Etter det kan du velge å legge til blant annet kurs, sertifisering, fagbrev, sertifikat og førerkort."
+                ]
+            , Melding.spørsmål
+                [ "Etter det kan du velge å legge til blant annet kurs, sertifisering, fagbrev, sertifikat og førerkort."
                 , "Er du klar til å begynne?"
                 ]
             ]
