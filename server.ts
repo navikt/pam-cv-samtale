60864979b329a57c8952588fec5eedff5a3597fd
Julian Torp <julian.torp@nav.no>
2020-06-16 10:28:13 +0200

    move amplitude tracker to server.ts

--- server.ts
+++ server.ts
@@ -9,7 +9,7 @@ import { RequestOptions } from 'http';
 import { Response } from 'express';
 import { NextFunction } from 'express';
 import session from "express-session";
-import logAmplitudeEvent from "./AmplitudeTracker";
+import amplitude from "amplitude-js";
 
 const getEnvironmentVariable = (key) => {
     if (!process.env[key]) {
@@ -28,6 +28,28 @@ const ENVIRONMENT_VARIABLES = {
 
 console.log(`API_GATEWAY_HOST: ${ENVIRONMENT_VARIABLES.API_GATEWAY_HOST}`);
 
+// ----- Amplitude
+const amplitudeTracker = (amplitudeToken, userId = null) => {
+    let amplitudeClient;
+    if (amplitudeToken) {
+        amplitudeClient = amplitude.getInstance();
+        amplitudeClient.init(
+            amplitudeToken, userId   , {
+                apiEndpoint: 'amplitude.nav.no/collect',
+                batchEvents: false,
+                includeReferrer: true,
+                includeUtm: true,
+                saveEvents: false
+            }
+        );
+    }
+    return amplitudeClient;
+};
+
+const logAmplitudeEvent = (sessionID, eventName, eventPayload, amplitudeToken) => {
+    amplitudeTracker(amplitudeToken, sessionID).logEvent(eventName, eventPayload);
+};
+// -----
 
 const server = express();
 
